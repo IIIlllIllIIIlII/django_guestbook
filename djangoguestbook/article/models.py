6b5829cd66a01785d10c275016d5c6ab8697fb86
IIIlllIllIIIlII <kjy02927@gmail.com>
2025-05-11 02:40:04 +0900

    feat. Article Model

--- djangoguestbook/article/models.py
+++ djangoguestbook/article/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class Article(models.Model):
+    nickname = models.CharField(max_length=10)
+    content = models.TextField(default="")
+    datetime = models.DateTimeField(auto_now_add=True)
